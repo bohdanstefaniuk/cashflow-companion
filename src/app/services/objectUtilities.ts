03fe252de452780826ea289001de294d6ef371a4
Bogdan <bogdanstefanjuk@gmail.com>
2019-12-10 17:29:50 +0200

    Add objectUtilities

---
+++ src/app/services/objectUtilities.ts
@@ -0,0 +1,28 @@
+export class ObjectUtilities {
+	static isObject(obj) {
+		var type = typeof obj;
+		return type === 'function' || type === 'object' && !!obj;
+	};
+
+	static copy<T>(target: T): T {
+		if (target === null) {
+		  return target;
+		}
+		if (target instanceof Date) {
+		  return new Date(target.getTime()) as any;
+		}
+		if (target instanceof Array) {
+		  const cp = [] as any[];
+		  (target as any[]).forEach((v) => { cp.push(v); });
+		  return cp.map((n: any) => this.copy<any>(n)) as any;
+		}
+		if (typeof target === 'object' && target !== {}) {
+		  const cp = { ...(target as { [key: string]: any }) } as { [key: string]: any };
+		  Object.keys(cp).forEach(k => {
+			cp[k] = this.copy<any>(cp[k]);
+		  });
+		  return cp as T;
+		}
+		return target;
+	  };
+}
